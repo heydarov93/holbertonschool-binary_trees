b900e20a5df1dece5a7427fd3f40e9d000fae153
Ravan Asad <ravanasad@outlook.com>
2024-01-13 11:59:56 -0800

    Updated logic

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,6 +12,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (!func)
 		return;
 	binary_tree_inorder(tree->left, func);
-	binary_tree_inorder(tree->right, func);
 	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
