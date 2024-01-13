f49294043d335a892b7653278431b1694c363d50
Ravan Asad <ravanasad@outlook.com>
2024-01-13 15:21:48 -0800

    Betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"   
+#include "binary_trees.h"
 
 /**
  * binary_tree_is_full - checks if a binary tree is full
@@ -7,11 +7,11 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (!tree->right && !tree->left)
-        return (1);
-    if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
-        return (1);
-    return (0);
+	if (!tree)
+		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
+	if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
+		return (1);
+	return (0);
 }
