a8cd46bf4ee3af8c5ae21a119567420883b95f24
heydarov93 <heyderov.yashar@mail.ru>
2024-01-14 08:14:41 -0800

    max height 1 binary tree case handled

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
@@ -6,28 +7,28 @@
  *
  * Return: 1 if the tree is perfect, 0 otherwise
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height == right_height)
 	{
-		return (1);
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
+
+		return (
+				binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right)
+			);
 	}
-	if (tree->left == NULL || tree->right == NULL)
-	{
-		return (0);
-	}
-	left = binary_tree_is_perfect(tree->left->left) +
-		binary_tree_is_perfect(tree->left->right);
-	right = binary_tree_is_perfect(tree->right->right) +
-		binary_tree_is_perfect(tree->right->left);
-	if (1 + left == 1 + right && left && right)
-	{
-		return (1);
-	}
+
 	return (0);
 }
 
