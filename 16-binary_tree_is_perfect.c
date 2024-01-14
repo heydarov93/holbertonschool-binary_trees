9309707fa3f0566af206616cb0c3b6a3d4c6e4fa
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-01-14 20:30:41 +0400

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,58 +1,24 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
 
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect
- * @tree: Pointer to the root node of the tree
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
  *
- * Return: 1 if the tree is perfect, 0 otherwise
+ * Return: 1 if tree is perfect, otherwise 0
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 
-	return ((binary_tree_balance(tree) == 0) ? 1 : 0);
-}
+	if (tree->right && tree->left)
+	{
+		if ((!tree->left->left && !tree->left->right) &&
+		    (!tree->right->left && !tree->right->right))
+			return (1);
 
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: ointer to the root node of the tree
- * Return: if tree NULL 0 else balance factor
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-}
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree
- * Return: If tree is NULL, your function must return 0
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (-1);
-
-	return (find_height(tree) - 1);
-}
-
-/**
- * find_height - finds the height of a binary tree
- * @tree: pointer to the root node of the tree
- * Return: height of tree
- */
-
-int find_height(const binary_tree_t *tree)
-{
-	int h_right = 0, h_left = 0;
-
-	if (!tree)
-		return (0);
-	h_left = find_height(tree->left);
-	h_right = find_height(tree->right);
-	return ((h_left > h_right) ? h_left + 1 : h_right + 1);
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
+	}
+	return (0);
 }
