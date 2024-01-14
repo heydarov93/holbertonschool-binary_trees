f0f7be3dd2fb94b6dc5115ac6659388a914f81c8
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-01-14 20:16:45 +0400

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,26 +8,49 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	if (!tree)
+		return (0);
 
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-	if (tree->left == NULL || tree->right == NULL)
-	{
-		return (0);
-	}
-	left = binary_tree_is_perfect(tree->left->left) +
-		binary_tree_is_perfect(tree->left->right);
-	right = binary_tree_is_perfect(tree->right->right) +
-		binary_tree_is_perfect(tree->right->left);
-	if (1 + left == 1 + right && left && right)
-	{
-		return (1);
-	}
-	return (0);
+	return ((binary_tree_balance(tree) == 0) ? 1 : 0);
 }
 
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: ointer to the root node of the tree
+ * Return: if tree NULL 0 else balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: If tree is NULL, your function must return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	return (find_height(tree) - 1);
+}
+
+/**
+ * find_height - finds the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of tree
+ */
+
+int find_height(const binary_tree_t *tree)
+{
+	int h_right = 0, h_left = 0;
+
+	if (!tree)
+		return (0);
+	h_left = find_height(tree->left);
+	h_right = find_height(tree->right);
+	return ((h_left > h_right) ? h_left + 1 : h_right + 1);
+}
